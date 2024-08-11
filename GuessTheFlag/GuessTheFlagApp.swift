467244e38360132ae5b45b888e738a9fddae6e9d
Devendra Kulkarni <devendrakulkarni@devendras-air.home>
2024-08-11 15:15:10 -0400

    Added IOS app GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Devendra Kulkarni on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
